c32837955ed35e88c9eaea4e6fffabd907db50a8
Andrei Minets <anminets@gmail.com>
2026-03-30 12:50:49 +0300

    Dev branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hi!!!!!!!!!!!</p>
-        <p>222</p>
-        <p>333</p>
-      </header>
-    </div>
+      <div>
+        <div>App</div>
+        <div>Dev branch commit 1</div>
+      </div>
   );
 }
-
 export default App;
