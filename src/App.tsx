58a26600a69d35453c64cd69908a868dbbcb10f1
Andrei Minets <anminets@gmail.com>
2026-03-30 13:52:26 +0300

    develop/popup branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div>
-        <div>App</div>
-        <div>Dev branch commit 1</div>
-      </div>
-  );
+    return (
+        <div>
+            <div>App</div>
+            <div>develop/popup branch commit 1</div>
+        </div>
+    );
 }
+
 export default App;
